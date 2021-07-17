abcb1c389a73806a3c807d1f2b926176f0d8caca
jasmine_r <joycesdaughter82@gmail.com>
2021-07-16 21:23:14 -0500

    Adding files

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const addGuestButton = document.querySelector(".invite");
+const guestInputLabel = document.querySelector(".add-guest label");
+const guestInput = document.querySelector(".add-guest input");
+const guestList = document.querySelector(".guest-list");
+const guestCount = document.querySelector(".attendance");
+const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+addGuestButton.addEventListener("click", function () {
+  const guest = guestInput.value;
+  // console.log(guest);
+  if (guest !== "") {
+    addToList(guest);
+    clearInput();
+    updateGuestCount();
+  }
+});
+
+const clearInput = function () {
+  guestInput.value = "";
+};
+
+const addToList = function (guest) {
+  const listItem = document.createElement("li");
+  listItem.innerText = guest;
+  guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+  const guests = document.querySelectorAll(".guest-list li");
+  guestCount.innerText = guests.length;
+  if (guests.length === 8) {
+    addGuestButton.classList.add("hide");
+    guestInput.classList.add("hide");
+    guestInputLabel.classList.add("hide");
+    guestFull.classList.remove("hide");
+  }
+};
+
+const assignItems = function () {
+  const potluckItems = [
+    "Chinese Chicken Salad Spring Rolls",
+    "Couscous Salad",
+    "Pasta Salad",
+    "Turkey and Cheddar sanadwiches",
+    "Salsa",
+    "Fruit Chaat",
+    "Sushi Rolls",
+    "Cupcakes",
+    "Ice cream cakes",
+    "cookies",
+    "lemonade",
+    "limeade"
+  ];
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}`;
+    assignedItems.append(listItem);
+    potluckItems.splice(randomPotluckIndex, 1);
+  }
+};
+
+assignButton.addEventListener("click", function () {
+  assignItems();
+  assignButton.disabled = true;
+});
